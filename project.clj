6dbb5f0895d42ab09447fa98511d951d75e00e1b
egr <egr@zengularity.com>
2015-12-11 00:55:06 +0100

    Update project groupName. Remove codox dependency

--- project.clj
+++ project.clj
@@ -1,12 +1,7 @@
-(defproject hiccup "1.0.5"
+(defproject ewen/hiccup "1.0.0"
   :description "A fast library for rendering HTML in Clojure"
-  :url "http://github.com/weavejester/hiccup"
+  :url "http://github.com/EwenG/hiccup"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :plugins [[codox "0.7.4"]]
-  :codox {:exclude [hiccup.compiler]
-          :sources ["src"]
-          :src-dir-uri "http://github.com/weavejester/hiccup/blob/1.0.5/"
-          :src-linenum-anchor-prefix "L"}
   :profiles
   {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
